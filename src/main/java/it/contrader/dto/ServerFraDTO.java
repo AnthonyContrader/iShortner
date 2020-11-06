45a20c4fb77df636313c9c46747287a69b4b1036
Francesco Defilippo <francesco.defilippo@studenti.unitn.it>
2020-11-06 10:07:36 +0100

    add Server Model, DTO, DAO, Converter

---
+++ src/main/java/it/contrader/dto/ServerFraDTO.java
@@ -0,0 +1,47 @@
+package it.contrader.dto;
+
+public class ServerFraDTO {
+
+	private int id;
+	private String nome_citta;
+	private int fk_id_url;
+	
+	ServerFraDTO(){
+		
+	}
+	
+	public ServerFraDTO(int id, String nome_citta, int fk_id_url){
+		this.id=id;
+		this.nome_citta=nome_citta;
+		this.fk_id_url=fk_id_url;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNomeCitta() {
+		return nome_citta;
+	}
+
+	public void setNomeCitta(String nomecitta) {
+		this.nome_citta = nomecitta;
+	}
+	
+	public int getFk() {
+		return this.fk_id_url;
+	}
+	
+	public void setFK(int fk_id_url) {
+		this.fk_id_url=fk_id_url;
+	}
+
+	
+	@Override
+	public String toString() {
+		return  id + "\t"  + nome_citta;
+	}
+}
