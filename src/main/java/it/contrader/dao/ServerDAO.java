d630e1396b8a0a4c74a2de4cd79472d39c2f2261
Cactus2405 <valeria.vacarciuc@hotmail.it>
2020-11-10 12:42:27 +0100

    chore: add DAO (server)

---
+++ src/main/java/it/contrader/dao/ServerDAO.java
@@ -0,0 +1,72 @@
+package it.contrader.dao;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.model.Server;
+import it.contrader.utils.ConnectionSingleton;
+
+public class ServerDAO {
+
+
+
+	private final static String QUERY_CREATE = "INSERT INTO server (posizione, tipologia, date, fk_idurl) VALUES (?,?,?,?)";
+	private final static String QUERY_READ = "SELECT * FROM server WHERE fk_idurl=?";
+	private final static String QUERY_SUPPORT_READ = "SELECT * FROM url WHERE fk_iduser=?";
+
+	public ServerDAO() {
+
+	}
+	
+
+	public static boolean insert(Server server) {
+		Connection connection = ConnectionSingleton.getInstance();
+		try {	
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_CREATE);
+			preparedStatement.setString(1, server.getPosizione());
+			preparedStatement.setString(2, server.getTipologia());
+			preparedStatement.setString(3, server.getData());
+			preparedStatement.setInt(4, server.getFkIdUrl());
+			preparedStatement.execute();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+
+	}
+     //CORRISPONDENZA 1 TO 1 OR 1 TO MANY?????
+	public static List<Server> read(int serverId) {
+		Connection connection = ConnectionSingleton.getInstance();
+		List<Server> serverList = new ArrayList<Server>();
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_SUPPORT_READ);
+			preparedStatement.setInt(1, serverId);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			PreparedStatement prep = connection.prepareStatement(QUERY_READ);
+			while(resultSet.next()) {
+				Server server = new Server();
+				int id = resultSet.getInt("id");
+				prep.setInt(1, id);
+				ResultSet result = prep.executeQuery();
+				result.next();
+				String posizione = result.getString("posizione");
+				String tipologia = result.getString("tipologia");
+				String date = result.getString("date");
+				int fk_idurl = result.getInt("fk_idurl");
+				server.setPosizione(posizione);
+				server.setTipologia(tipologia);
+				server.setData(date);
+				server.setFkIdUrl(fk_idurl);
+				serverList.add(server);
+			}
+			return serverList;
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+}
