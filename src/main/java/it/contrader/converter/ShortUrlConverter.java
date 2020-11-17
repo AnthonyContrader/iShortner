65e6be951fbe9a861e53861e77070c36a90f09e8
Matteo <matteogis.92@gmail.com>
2020-11-17 16:52:01 +0100

    Aggiunta delle entità shorturl, service shorturl e homepage user

---
+++ src/main/java/it/contrader/converter/ShortUrlConverter.java
@@ -0,0 +1,32 @@
+package it.contrader.converter;
+
+import org.springframework.stereotype.Component;
+
+import it.contrader.dto.ShortUrlDTO;
+import it.contrader.dto.UserDTO;
+import it.contrader.model.ShortUrl;
+import it.contrader.model.User;
+
+@Component
+public class ShortUrlConverter extends AbstractConverter<ShortUrl, ShortUrlDTO>{
+
+	@Override
+	public ShortUrl toEntity(ShortUrlDTO dto) {
+		ShortUrl url = null;
+		if (dto != null) {
+			url = new ShortUrl(dto.getId(), dto.getLongurl(), dto.getShorturl(), dto.getFk_url());
+		}
+		return url;
+	}
+
+	@Override
+	public ShortUrlDTO toDTO(ShortUrl entity) {
+		ShortUrlDTO urlDTO = null;
+		if (entity != null) {
+			urlDTO = new ShortUrlDTO(entity.getId(), entity.getLongurl(), entity.getShorturl(), entity.getFk_url());
+
+		}
+		return urlDTO;
+	}
+
+}
