60b7554714464eb4eba3533b96c0c1f4b81f80a5
Matteo <matteogis.92@gmail.com>
2020-12-02 15:37:46 +0100

    Initial version of Stats generated by JHipster-6.10.5

---
+++ Stats/src/main/java/com/ishortner/stats/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.ishortner.stats.config.audit;
---
+++ Stats/src/main/java/com/ishortner/stats/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.ishortner.stats.web.rest.vm;
